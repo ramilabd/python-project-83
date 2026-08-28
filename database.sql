f411258287c7c3a02e185a1f0d5d23d35f5fcefc
Ramil <ramsmart@mail.ru>
2026-08-28 18:51:28 +0500

    Add url_checks table schema

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS urls (
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE NOT NULL DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls (id) NOT NULL,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at DATE NOT NULL DEFAULT now()
+);
